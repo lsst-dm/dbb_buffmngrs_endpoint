8db1b4a9b9905905875515bfb350ac37a9628058
Mikolaj Kowalik <mxk@illinois.edu>
2020-06-11 14:44:21 -0500

    Add support for Gen3 Butler ingest
    Added a plugin allowing an Ingester to ingest images to a Gen3 Butler
    repository.

--- python/lsst/dbb/buffmngrs/endpoint/plugins.py
+++ python/lsst/dbb/buffmngrs/endpoint/plugins.py
@@ -20,9 +20,12 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import importlib
 import logging
+import lsst.daf.butler as butler
 import lsst.pipe.tasks.ingest as ingest
+import lsst.obs.base as base
 
-__all__ = ["NullIngest", "Gen2Ingest"]
+
+__all__ = ["NullIngest", "Gen2Ingest", "Gen3Ingest"]
 
 
 logger = logging.getLogger(__name__)
@@ -71,7 +74,7 @@ class Gen2Ingest(object):
         self.task = ingest.IngestTask.prepareTask(root, **opts)
 
         pkg = importlib.import_module(ingest.__package__)
-        self._version = "not available"
+        self._version = "N/A"
         try:
             self._version = getattr(pkg, "__version__")
         except AttributeError:
@@ -96,3 +99,63 @@ class Gen2Ingest(object):
             Path to the file.
         """
         self.task.ingestFiles(filename)
+
+
+class Gen3Ingest(object):
+    """Ingest a file to Gen2 Butler repository.
+
+    Parameters
+    ----------
+    config : `dict`
+
+
+    Raises
+    ------
+    ValueError
+        If root directory of the Bulter repository is not provided.
+    """
+
+    def __init__(self, config):
+        required = {"root"}
+        missing = required - set(config)
+        if missing:
+            msg = f"invalid configuration: {', '.join(missing)} not provide."
+            logger.critical(msg)
+            raise ValueError(msg)
+        root = config["root"]
+
+        run = config.get("run", "test")
+
+        opts = dict(run=run, writeable=True)
+        btl = butler.Butler(root, **opts)
+
+        cfg = base.RawIngestConfig()
+        cfg.transfer = config.get("transfer", "symlink")
+        self.task = base.RawIngestTask(config=cfg, butler=btl)
+
+        pkg = importlib.import_module(ingest.__package__)
+        self._version = "N/A"
+        try:
+            self._version = getattr(pkg, "__version__")
+        except AttributeError:
+            logger.warning("failed to identified plugin version")
+
+    def version(self):
+        """Return the version of the LSST ingest task.
+
+        Returns
+        -------
+        `str`
+            Version of the LSST ingest task used for the ingest attempt.
+        """
+        return self._version
+
+    def execute(self, filename):
+        """Make an attempt to ingest the file.
+
+        Parameters
+        ----------
+        filename : `str`
+            Path to the file.
+        """
+        self.task.run([filename])  # run() requires a list as an argument
